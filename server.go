9ac1ebbcd7105c787c761f555853c2c29dc84bf1
zhuchunshu <laravel@88.com>
2023-03-01 18:12:20 +0800

    Init Commit

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		JSONEncoder: json.Marshal,
+		JSONDecoder: json.Unmarshal,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		return
+	}
+}
